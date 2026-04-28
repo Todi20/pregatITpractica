59d6b96ab130fb2a5f4fd2f6d58d6765a2c498d4
Liviu <liviutodi20@gmail.com>
2026-04-28 16:12:30 +0300

    Number Classification

---
+++ pregatIT/src/Sesiunea2/modul1/Countdown.java
@@ -0,0 +1,17 @@
+package Sesiunea2.modul1;
+
+public class Countdown {
+    public static void main(String[] args) {
+
+        int n = 19;
+
+
+
+        while(n>=1){
+            System.out.println(n);
+            n--;
+        }
+
+
+    }
+}
