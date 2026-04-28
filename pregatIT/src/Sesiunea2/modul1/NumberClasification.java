096a25c82eba0aeee7d55a45165c2eac828b41c6
Todi20 <liviutodi20@gmail.com>
2026-04-28 15:55:14 +0300

    Number Classification

---
+++ pregatIT/src/Sesiunea2/modul1/NumberClasification.java
@@ -0,0 +1,21 @@
+package Sesiunea2.modul1;
+
+public class NumberClasification {
+    public static void main(String[] args){
+        int number = 19;
+
+        if(number > 0 ){
+            System.out.println("Pozitiv");
+        }
+
+        else if (number <0){
+            System.out.println("negativ");
+        }
+
+        else{
+            System.out.println("Zero");
+        }
+
+
+    }
+}
